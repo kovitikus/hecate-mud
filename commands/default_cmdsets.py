5c4f83b881584014193aa33e60a64ae1293b51fd
Kovitikus <kovitikus@gmail.com>
2019-09-05 00:30:01 -0400

    remove custom create cmd from cmdset

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -42,7 +42,6 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         self.add(command.CmdGrantSP())
         self.add(command.CmdTest())
         self.add(command.CmdLook())
-        self.add(command.CmdCreate())
         self.add(command.CmdInventory())
         self.add(command.CmdGet())
         self.add(command.CmdInhand())
