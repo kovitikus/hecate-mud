c8071c3f951301c789c61b0a1cef75ef2a4082ed
Kovitikus <kovitikus@gmail.com>
2019-09-21 02:54:10 -0400

    OOC Common Batchcode

---
+++ world/OOC_commons.py
@@ -0,0 +1,11 @@
+#HEADER
+from evennia.utils.create import create_object
+
+#CODE
+caller.msg("Creating the Common Room.")
+common_room = create_object(typeclass="typeclasses.rooms.OOC_Room", 
+                            key='Common Room', report_to=caller)
+if DEBUG:
+    common_room.delete()
+    caller.msg("Common Room deleted!")
+    caller.msg("DEBUG COMPLETE!")
