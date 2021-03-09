4896253d82b5c6058cfecb81ce6f2970dad51766
Kovitikus <kovitikus@gmail.com>
2021-03-09 00:50:48 -0500

    Added CmdAbandonFailedTraveller. #39

--- travel/travel_cmdset.py
+++ travel/travel_cmdset.py
@@ -5,11 +5,12 @@ from travel import travel_cmds
 class travelCmdSet(CmdSet):
     key = "card_dir"
     def at_cmdset_creation(self):
-        self.add(travel_cmds.North())
-        self.add(travel_cmds.Northeast())
-        self.add(travel_cmds.East())
-        self.add(travel_cmds.Southeast())
-        self.add(travel_cmds.South())
-        self.add(travel_cmds.Southwest())
-        self.add(travel_cmds.West())
-        self.add(travel_cmds.Northwest())
+        self.add(travel_cmds.CmdNorth())
+        self.add(travel_cmds.CmdNortheast())
+        self.add(travel_cmds.CmdEast())
+        self.add(travel_cmds.CmdSoutheast())
+        self.add(travel_cmds.CmdSouth())
+        self.add(travel_cmds.CmdSouthwest())
+        self.add(travel_cmds.CmdWest())
+        self.add(travel_cmds.CmdNorthwest())
+        self.add(travel_cmds.CmdAbandonFailedTraveller())
