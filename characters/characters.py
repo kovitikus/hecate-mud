d4c644bd9d2a45741e399cc0d19d81b4855f50e5
Kovitikus <kovitikus@gmail.com>
2021-03-15 23:17:28 -0400

    Added AFK status, StatusHandler, and adjusted line lengths. #14

--- characters/characters.py
+++ characters/characters.py
@@ -9,35 +9,32 @@ from items.item_handler import ItemHandler
 from characters.equipment_handler import EquipmentHandler
 from characters.inventory_handler import InventoryHandler
 from characters.currency_handler import CurrencyHandler
+from characters.status_handler import StatusHandler
 from travel.travel_handler import TravelHandler
 
 
 class Character(DefaultCharacter):
-
+    @lazy_property
+    def status(self):
+        return StatusHandler(self)
     @lazy_property
     def travel(self):
         return TravelHandler(self)
-
     @lazy_property
     def skill(self):
         return SkillHandler(self)
-
     @lazy_property
     def combat(self):
         return CombatHandler(self)
-
     @lazy_property
     def inv(self):
         return InventoryHandler(self)
-        
     @lazy_property
     def equip(self):
         return EquipmentHandler(self)
-
     @lazy_property
     def item(self):
         return ItemHandler(self)
-
     @lazy_property
     def currency(self):
         return CurrencyHandler(self)
@@ -48,16 +45,21 @@ class Character(DefaultCharacter):
 
         # Stats
         self.attributes.add('hp', {'max_hp': 100, 'current_hp': 100})
-        self.attributes.add('inventory_slots', {'max_slots': 0, 'occupied_slots': 0})
-        self.attributes.add('coin', {'plat': 0, 'gold': 0, 'silver': 0, 'copper': 0})
-        equip_dic = {'head': None, 'neck': None, 'shoulders': None, 'chest': None, 'arms': None, 'hands': None,
-                            'fingers': None, 'waist': None, 'thighs': None, 'calves': None, 
-                            'feet': None, 'inventory_container': None}
+        self.attributes.add('inventory_slots', 
+            {'max_slots': 0, 'occupied_slots': 0})
+        self.attributes.add('coin', 
+            {'plat': 0, 'gold': 0, 'silver': 0, 'copper': 0})
+        equip_dic = {'head': None, 'neck': None, 'shoulders': None, 
+            'chest': None, 'arms': None, 'hands': None, 'fingers': None, 
+            'waist': None, 'thighs': None, 'calves': None, 'feet': None, 
+        'inventory_container': None}
         self.attributes.add('equipment', equip_dic)
 
         # Avoids execution of this code on initial setup of superuser.
-        # Prototype spawner attempts to set objects' home specifically to DEFAULT_HOME, which is Limbo.
-        # In the initial_setup module, Limbo isn't created until after the superuser. https://pastebin.com/Qs4gwwZe
+        # Prototype spawner attempts to set objects' home specifically
+        # to DEFAULT_HOME, which is Limbo.
+        # In the initial_setup module, Limbo isn't created until after
+        # the superuser. https://pastebin.com/Qs4gwwZe
         if not self.dbref == '#1':
             self.equip.generate_equipment()
 
@@ -73,14 +75,17 @@ class Character(DefaultCharacter):
         self.attributes.add('busy', False)
         self.attributes.add('hands', {'main': None, 'off': None})
         self.attributes.add('hands_desc', {'main': 'right', 'off': 'left'})
-        self.attributes.add('wielding', {'main': None, 'off': None, 'both': None})
+        self.attributes.add('wielding', {'main': None, 'off': None, 
+                            'both': None})
         self.attributes.add('stance', None)
         self.attributes.add('standing', True)
         self.attributes.add('kneeling', False)
         self.attributes.add('sitting', False)
         self.attributes.add('lying', False)
+        self.attributes.add('afk_timer', 600) # 10 minute timeout
 
-    def announce_move_from(self, destination, msg=None, mapping=None, **kwargs):
+    def announce_move_from(self, destination, msg=None, mapping=None, 
+                            **kwargs):
         """
         Called if the move is to be announced. This is
         called while we are still standing in the old
@@ -99,25 +104,29 @@ class Character(DefaultCharacter):
             origin: the location of the object before the move.
             destination: the location of the object after moving.
         """
-        # TODO: Check if the char is teleported out of a room with no exits.
+        # TODO: Check if the char is teleported out of a room with 
+        #                                                   no exits.
         # It currently gives an error:
         #
-        # Couldn't perform move ('at_announce_move()'). Contact an admin. ('TravelHandler' object has no
-        # attribute 'exit_obj')
+        # Couldn't perform move ('at_announce_move()'). 
+        # Contact an admin. ('TravelHandler' object has no 
+    #                                           attribute 'exit_obj')
         if not self.location:
             return
         location = self.location
         origin = location or "nowhere"
 
-        # Tells the handler to find an exit and store that exit on itself as self.exit_obj
+        # Tells the handler to find an exit and store that exit on 
+        # itself as self.exit_obj
         self.travel.find_exit_by_destination(destination)
         if self.travel.travel_one_way(): # If the player is teleported.
             return
-        self.travel.pick_departure_string() #Chooses the departing exit string.
+        self.travel.pick_departure_string()
         self.travel.send_departure_string()
         
 
-    def announce_move_to(self, source_location, msg=None, mapping=None, **kwargs):
+    def announce_move_to(self, source_location, msg=None, mapping=None, 
+                        **kwargs):
         """
         Called after the move if the move was not quiet. At this point
         we are standing in the new location.
@@ -136,12 +145,14 @@ class Character(DefaultCharacter):
                 origin: the location of the object before the move.
                 destination: the location of the object after moving.
         """
-        #TODO: Add contents of hands and wielding description for arriving characters.
+        #TODO: Add contents of hands and wielding description for 
+        #                                           arriving characters.
 
         if not source_location and self.location.has_account:
             # This was created from nowhere and added to an account's
             # inventory; it's probably the result of a create command.
-            string = f"You now have {self.get_display_name(self.location)} in your possession." 
+            string = (f"You now have {self.get_display_name(self.location)} " 
+                        f"in your possession.")
             self.location.msg(string)
             return
 
@@ -152,7 +163,8 @@ class Character(DefaultCharacter):
         if origin:
             self.travel.find_origin_exit(origin, location)
             
-        # If the player is teleported or travels through a 1 way exit, give a generic announcement.
+        # If the player is teleported or travels through a 1 way exit, 
+        # give a generic announcement.
         if self.travel.origin_exit_missing_destination(origin, location):
             return
 
@@ -162,17 +174,21 @@ class Character(DefaultCharacter):
         
     def at_after_move(self, source_location):
         """
-        Once the self object has settled in at it's new location, this hook is called.
+        Once the self object has settled in at it's new location, 
+        this hook is called.
 
-        This hook's default behavior is to look at the room, returning the desc attribute on the room to the self object.
+        This hook's default behavior is to look at the room, returning 
+        the desc attribute on the room to the self object.
         ---
-        I've instead changed it to call an attribute on the room that holds a short description.
-        This is a custom system that returns an abbrevieted glance at the room and the most critical information.
+        I've instead changed it to call an attribute on the room that
+        holds a short description.
+        This is a custom system that returns an abbrevieted glance at
+        the room and the most critical information.
 
         Example:
-            You arrive at <destination name>. <Person/NPC> <is/are> here. You see <exit name> to the 
-                                                    <exit direction>, an <exit name> to the <exit direction>,
-                                                                        and <exit name> to the <exit direction>.
+            You arrive at <destination name>. <Person/NPC> <is/are> here.
+            You see <exit name> to the <exit direction>, an <exit name>
+            to the <exit direction>, and <exit name> to the <exit direction>.
         """
         # Force the character to be greeted with the room's short description.
         if not source_location:
