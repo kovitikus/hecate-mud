79cc8e2a23ed650f926df1c28f7f8740c3823d10
Kovitikus <kovitikus@gmail.com>
2021-04-18 10:44:27 -0400

    Initiated the module. #43

---
+++ mobs/mob_spawn_handler.py
@@ -0,0 +1,14 @@
+from evennia import utils
+from evennia import GLOBAL_SCRIPTS
+from evennia.prototypes.spawner import spawn
+
+from misc import general_mechanics as gen_mec
+
+class MobSpawnHandler:
+    def __init__(self, owner):
+        # The owner is the zone script that stores spawned mob data.
+        self.owner = owner
+
+        # This handler lives on the script that was generated the first time this zone was occupied.
+        # The name of the script is set as the same unique identifier of the zone on the ledger.
+        self.zone_ledger_uid = GLOBAL_SCRIPTS.zone_ledger.attributes.get(owner.key)
