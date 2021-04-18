e350fbcd0d447e3fffdcb537c65428a30b77fc40
Kovitikus <kovitikus@gmail.com>
2021-04-18 19:49:48 -0400

    Default starting method initialized. #43

--- mobs/mob_spawn_handler.py
+++ mobs/mob_spawn_handler.py
@@ -12,3 +12,6 @@ class MobSpawnHandler:
         # This handler lives on the script that was generated the first time this zone was occupied.
         # The name of the script is set as the same unique identifier of the zone on the ledger.
         self.zone_ledger_uid = GLOBAL_SCRIPTS.zone_ledger.attributes.get(owner.key)
+    
+    def start_spawner(self):
+        pass
