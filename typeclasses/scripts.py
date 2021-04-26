51e1c68d924cee201389557ab070f1b81b73bf8f
Kovitikus <kovitikus@gmail.com>
2021-04-26 07:55:34 -0400

    Updated handler to simply SpawnHandler and moved file location to room directory. #43

--- typeclasses/scripts.py
+++ typeclasses/scripts.py
@@ -1,9 +1,9 @@
 from evennia import DefaultScript
 from evennia.utils.utils import lazy_property
 
-from mobs.mob_spawn_handler import MobSpawnHandler
+from rooms.spawn_handler import SpawnHandler
 
 class Script(DefaultScript):
     @lazy_property
     def spawn(self):
-        return MobSpawnHandler(self)
+        return SpawnHandler(self)
