9e8e4105862864c68e3caeec38b695b26bc005bc
Kovitikus <kovitikus@gmail.com>
2021-05-10 15:51:09 -0400

    Separated the sentients from the zone pools. #43

--- sentients/sentients.py
+++ sentients/sentients.py
@@ -12,12 +12,31 @@ BASIC_SUPPLIES = {
     'stock': {'crudely_made_torch': 10, 'foul_smelling_bait': 100}
 }
 
-sewer = {
-    'rat': {
-        'adj1': ['vicious', 'slick-coated', 'large', 'red-maned', 'filthy', 'feral'],
-        'adj2': ['black', 'yellow', 'pale-white', 'black'],
-        'noun': 'rat',
-        'health': 100,
-        'base_difficulty': 0,
-    }
+
+# Critters
+rat = {
+    'adj1': ['vicious', 'slick-coated', 'large', 'red-maned', 'filthy', 'feral'],
+    'adj2': ['black', 'yellow', 'pale-white', 'black'],
+    'noun': 'rat',
+    'health': 100,
+    'base_difficulty': 0
 }
+
+spider = {
+    'adj1': ['vicious', 'slick-coated', 'large', 'red-maned', 'filthy', 'feral'],
+    'adj2': ['black', 'yellow', 'pale-white', 'black'],
+    'noun': 'spider',
+    'health': 100,
+    'base_difficulty': 0
+}
+
+snake = {
+    'adj1': ['vicious', 'large', 'filthy', 'feral'],
+    'adj2': ['black', 'yellow', 'pale-white', 'black', 'green', 'emerald', 'jade', 'ruby'],
+    'noun': 'snake',
+    'health': 100,
+    'base_difficulty': 0
+}
+
+# Zone Spawn Pools
+sewer = {'rat': rat, 'spider': spider, 'snake': snake}
