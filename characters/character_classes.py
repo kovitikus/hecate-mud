440fb486acec3b7f424c36c7a041d1b79c8d0aee
Kovitikus <kovitikus@gmail.com>
2021-06-29 04:51:21 -0400

    Updated character class format.

--- characters/character_classes.py
+++ characters/character_classes.py
@@ -1,8 +1,14 @@
-# Starting Classes
-mage = {'skillsets': ['fire', 'staves'], 'armor_type': 'cloth'}
+# Main Classes
+main_classes = {
+    'mage': {
+        'skillsets': ['fire', 'staves'], 'armor_type': 'cloth'
+        },
 
-paladin = {'skillsets': ['holy'], 'armor_type': 'plate'}
+    'paladin': {
+        'skillsets': ['holy'], 'armor_type': 'plate'
+        },
 
-priest = {'skillsets': ['holy'], 'armor_type': 'cloth'}
-
-starting_classes = [mage, paladin, priest]
+    'priest': {
+        'skillsets': ['holy'], 'armor_type': 'cloth'
+        }
+}
