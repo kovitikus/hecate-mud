61d7116cb75286a1ee01c855cb80d10eeedbdf11
Kovitikus <kovitikus@gmail.com>
2021-08-27 06:23:41 -0400

    Added unit tests for the currency handler.

--- characters/test_currency.py
+++ characters/test_currency.py
@@ -1,7 +1,20 @@
+from unittest import result
 from evennia.utils.create import create_object
 from misc.test_resources import HecateTest
 
 class TestCurrency(HecateTest):
+    def setUp(self):
+        super().setUp()
+        self.coin_dict1 = {'plat': 27, 'gold': 48, 'silver': 125, 'copper': 88}
+        self.coin_dict2 = {'plat': 233, 'gold': 0, 'silver': 41, 'copper': 0}
+
+    def test_return_all_coin_types(self):
+        plat, gold, silver, copper = self.char1.currency.return_all_coin_types(coin_dict=self.coin_dict1)
+        self.assertEqual( 27, plat)
+        self.assertEqual( 48, gold)
+        self.assertEqual( 125, silver)
+        self.assertEqual( 88, copper)
+
     def test_copper_to_coin_dict(self):
         copper = 8323
         coin_dict = self.char1.currency.copper_to_coin_dict(copper)
@@ -9,3 +22,114 @@ class TestCurrency(HecateTest):
         self.assertEqual(8, coin_dict['silver'])
         self.assertEqual(0, coin_dict['gold'])
         self.assertEqual(0, coin_dict['plat'])
+
+    def test_all_coin_types_to_string(self):
+        coin_str = (f"{self.coin_dict1['plat']}p {self.coin_dict1['gold']}g "
+            f"{self.coin_dict1['silver']}s {self.coin_dict1['copper']}c")
+        result_str = self.char1.currency.all_coin_types_to_string(self.coin_dict1)
+        self.assertEqual(coin_str, result_str)
+
+    def test_positive_coin_types_to_string(self):
+        coin_str = f"{self.coin_dict2['plat']}p {self.coin_dict2['silver']}s"
+        result_str = self.char1.currency.positive_coin_types_to_string(self.coin_dict2)
+        self.assertEqual(coin_str, result_str)
+
+    def test_add_coin_to_owner(self):
+        char1 = self.char1
+        coin_dict1 = self.coin_dict1
+        coin_dict2 = self.coin_dict2
+
+        char1.db.coin = coin_dict1
+        self.assertEqual(coin_dict1, char1.db.coin)
+
+        char1.currency.add_coin_to_owner(coin_dict2)
+
+        plat = coin_dict1['plat'] + coin_dict2['plat']
+        gold = coin_dict1['gold'] + coin_dict2['gold']
+        silver = coin_dict1['silver'] + coin_dict2['silver']
+        copper = coin_dict1['copper'] + coin_dict2['copper']
+
+        self.assertEqual(plat, char1.db.coin['plat'])
+        self.assertEqual(gold, char1.db.coin['gold'])
+        self.assertEqual(silver, char1.db.coin['silver'])
+        self.assertEqual(copper, char1.db.coin['copper'])
+
+    def test_add_coin(self):
+        coin_dict1 = self.coin_dict1
+        coin_dict2 = self.coin_dict2
+
+        result_coin_dict = self.char1.currency.add_coin(coin_dict1, coin_dict2)
+
+        plat = coin_dict1['plat'] + coin_dict2['plat']
+        gold = coin_dict1['gold'] + coin_dict2['gold']
+        silver = coin_dict1['silver'] + coin_dict2['silver']
+        copper = coin_dict1['copper'] + coin_dict2['copper']
+
+        self.assertEqual(plat, result_coin_dict['plat'])
+        self.assertEqual(gold, result_coin_dict['gold'])
+        self.assertEqual(silver, result_coin_dict['silver'])
+        self.assertEqual(copper, result_coin_dict['copper'])
+
+    def test_balance_coin_dict(self):
+        unbalanced_coin_dict = {'plat': 2358, 'gold': 1280, 'silver': 998, 'copper': 2200}
+        balanced_coin_dict = {'plat': 2359, 'gold': 281, 'silver': 0, 'copper': 200}
+
+        result_coin_dict = self.char1.currency.balance_coin_dict(unbalanced_coin_dict)
+        self.assertEqual(balanced_coin_dict['plat'], result_coin_dict['plat'])
+        self.assertEqual(balanced_coin_dict['gold'], result_coin_dict['gold'])
+        self.assertEqual(balanced_coin_dict['silver'], result_coin_dict['silver'])
+        self.assertEqual(balanced_coin_dict['copper'], result_coin_dict['copper'])
+
+    def test_convert_coin_type(self):
+        plat = 2
+        gold = 2
+        silver = 2
+        copper = 2
+
+        plat_result = self.char1.currency.convert_coin_type(plat=plat, gold=gold, silver=silver,
+            copper=copper, result_type='plat')
+        self.assertEqual(2.002002002, plat_result)
+
+        gold_result = self.char1.currency.convert_coin_type(plat=plat, gold=gold, silver=silver,
+            copper=copper, result_type='gold')
+        self.assertEqual(2_002.002002, gold_result)
+
+        silver_result = self.char1.currency.convert_coin_type(plat=plat, gold=gold, silver=silver,
+            copper=copper, result_type='silver')
+        self.assertEqual(2_002_002.002, silver_result)
+
+        copper_result = self.char1.currency.convert_coin_type(plat=plat, gold=gold, silver=silver,
+            copper=copper)
+        self.assertEqual(2_002_002_002, copper_result)
+
+    def test_create_coin_dict(self):
+        plat = 55
+        gold = 55
+        silver = 55
+        copper = 55
+
+        result_dict = self.char1.currency.create_coin_dict(plat=plat, gold=gold, silver=silver,
+            copper=copper)
+        self.assertEqual(plat, result_dict['plat'])
+        self.assertEqual(gold, result_dict['gold'])
+        self.assertEqual(silver, result_dict['silver'])
+        self.assertEqual(copper, result_dict['copper'])
+
+    def test_coin_dict_to_copper(self):
+        coin_dict = {}
+        coin_dict['plat'] = 2
+        coin_dict['gold'] = 2
+        coin_dict['silver'] = 2
+        coin_dict['copper'] = 2
+
+        copper_result = self.char1.currency.coin_dict_to_copper(coin_dict)
+        self.assertEqual(2_002_002_002, copper_result)
+
+    def test_copper_to_coin_dict(self):
+        copper = 2_002_002_002
+
+        result_dict = self.char1.currency.copper_to_coin_dict(copper)
+        self.assertEqual(2, result_dict['plat'])
+        self.assertEqual(2, result_dict['gold'])
+        self.assertEqual(2, result_dict['silver'])
+        self.assertEqual(2, result_dict['copper'])
